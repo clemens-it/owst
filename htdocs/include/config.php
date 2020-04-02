2c16e187f2ad2a57d566d4b8deaece5f1fce95c3
Clemens Egger <clemens_e@gmx.net>
2020-04-02 10:01:32 +0200

    Update config not to use sudo model anymore
    With a dedicated PHP FPM process using sudo is not necessary anymore.

--- htdocs/include/config.php
+++ htdocs/include/config.php
@@ -18,31 +18,40 @@
 	$cfg['forever_valid_from'] = '1999-01-01';
 	$cfg['forever_valid_until'] = '2099-01-01';
 
-
 	//set if you want logging. if the setting contains no slashes it will be
 	//used as a tag and the message will go to syslog. if log_file is empty no logging occurs
 	$cfg['log_file']  = $cfg['logdir']. '/owst.log';
 	$cfg['lock_file'] = $cfg['libdir']. '/owst.lock';
 
-	//at user - user which ownes the AT queue
-	//a corresponding line in /etc/sudoers is required for www-data to show the AT queue:
-	// www-data ALL = (pi) NOPASSWD: /usr/bin/atq
-	$cfg['at_user'] = 'pi';
-
 	//at command line
-	//$cfg['at_cmd_line'] = "echo $cfg_executable | at ";
 	//At tries to get back into the current working directory at execution time. if it takes
 	//the cwd of the web script, at will fail at execution because the at_user might not have
-	//the rights to access the directory of the web script
-	$cfg['at_cmd_line'] = "cd /; echo $cfg_executable | sudo -nu ".escapeshellarg($cfg['at_user']) ." at ";
+	//the rights to access the directory of the web script. therefore the 'cd /'
+	$cfg['at_cmd_line'] = "cd /; echo $cfg_executable | at ";
 
-	//$cfg['atq_cmd_line'] = "atq | sort -k6n -k3M -k4n -k5n";
-	$cfg['atq_cmd_line'] = "(sudo -nu ".escapeshellarg($cfg['at_user']) ." atq | sort -k6n -k3M -k4n -k5n) 2>&1";
+	//atq command line
+	$cfg['atq_cmd_line'] = "(atq | sort -k6n -k3M -k4n -k5n) 2>&1";
 
-	//$cfg['atempty_cmd_line'] = "atq | cut -f1 | xargs --no-run-if-empty atrm";
-	//(sudo -nu pi atq | cut -f1 | xargs --no-run-if-empty sudo -nu pi atrm) 2>&1
-	$cfg['atempty_cmd_line'] = "(sudo -nu ".escapeshellarg($cfg['at_user']) ." atq | cut -f1 | ".
-			"xargs --no-run-if-empty sudo -nu ".escapeshellarg($cfg['at_user']) ." atrm) 2>&1";
+	//empty at queue command line
+	$cfg['atempty_cmd_line'] = "(atq | cut -f1 | xargs --no-run-if-empty atrm) 2>&1";
+
+
+	//at user - user which owns the AT queue
+	//a corresponding line in /etc/sudoers is required for www-data to show the AT queue:
+	// www-data ALL = (owst) NOPASSWD: /usr/bin/atq, /usr/bin/atrm, /usr/bin/at
+	//only required if using sudo model
+	//$cfg['at_user'] = 'owst';
+
+	//at_cmd_line using sudo model
+	//$cfg['at_cmd_line'] = "cd /; echo $cfg_executable | sudo -nu ".escapeshellarg($cfg['at_user']) ." at ";
+
+	//atq_cmd_line using sudo model
+	//$cfg['atq_cmd_line'] = "(sudo -nu ".escapeshellarg($cfg['at_user']) ." atq | sort -k6n -k3M -k4n -k5n) 2>&1";
+
+	//atempty_cmd_line using sudo model
+	//$cfg['atempty_cmd_line'] = "(sudo -nu ".escapeshellarg($cfg['at_user']) ." atq | cut -f1 | ".
+	//		"xargs --no-run-if-empty sudo -nu ".escapeshellarg($cfg['at_user']) ." atrm) 2>&1";
+
 
 	// ------------ usually there should be no need to make any changes below here ------------ //
 	$cfg['use_syslog'] = ($cfg['log_file'] != '' && strstr($cfg['log_file'], '/') === FALSE);
