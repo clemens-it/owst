488d277e69b1ace40ecba63ab161c31327ff892a
Clemens Egger <clemens.egger@eurac.edu>
2024-05-28 22:06:56 +0200

    Refactor code, change indentation to spaces

--- cli/tp.php
+++ cli/tp.php
@@ -4,57 +4,60 @@
 You will be needing the following deb packages: owserver libow-php5 php5-cli sqlite3 php5-sqlite at
 Optional: ow-shell owfs owhttp
 */
-	if(!isset($argc))
-		die("not running from the command line\n");
 
-	chdir(__DIR__);
+if (!isset($argc)) {
+    die("not running from the command line\n");
+}
 
-	//define variable with current filename to pass to config.php
-	$cfg_executable = __FILE__;
-	require_once 'include/config.php';
-	require_once 'include/log.php';
-	require_once 'include/functions.php';
-	//require_once 'include/load_php_OW.php'; //loads OW extension and/or checks if its loaded
+chdir(__DIR__);
 
-	//log_levels 0.. none, 1.. some, 5.. all. error, warn, info, debug
-	$log_level = LLDEBUG;
-	//$log_level = LLINFO;
+//define variable with current filename to pass to config.php
+$cfg_executable = __FILE__;
+require_once 'include/config.php';
+require_once 'include/log.php';
+require_once 'include/functions.php';
+//require_once 'include/load_php_OW.php'; //loads OW extension and/or checks if its loaded
 
-	logEvent("program ".__FILE__." started. Cmd line: ". implode(" ", $argv), LLINFO);
+//log_levels 0.. none, 1.. some, 5.. all. error, warn, info, debug
+$log_level = LLDEBUG;
+//$log_level = LLINFO;
 
-	//handle command line options
-	$shopt = 'mrh';
-	$longopt = array(
-		'modechange', 'reprogram-all', 'help');
-	$opt = getopt($shopt, $longopt);
+logEvent("program " . __FILE__ . " started. Cmd line: " . implode(" ", $argv), LLINFO);
 
-	if (isset($opt['h']) || isset($opt['help'])) {
-		displayHelp();
-		exit(0);
-	}
-	$copt = array();
-	if (isset($opt['r']) || isset($opt['reprogram-all']))
-		$copt['reprogram_at'] = 1;
-	if (isset($opt['m']) || isset($opt['modechange']))
-		$copt['modechange'] = 1;
+//handle command line options
+$shopt = 'mrh';
+$longopt = [
+    'modechange', 'reprogram-all', 'help'
+];
+$opt = getopt($shopt, $longopt);
 
-	//one wire: init
-	if (!init($cfg['ow_adapter'])) {
-		logEvent("cannot initialize 1-wire bus", LLWARN);
-		print("cannot initialize 1-wire bus.\n");
-		exit(9);
-	}
+if (isset($opt['h']) || isset($opt['help'])) {
+    displayHelp();
+    exit(0);
+}
+$copt = array();
+if (isset($opt['r']) || isset($opt['reprogram-all'])) {
+    $copt['reprogram_at'] = 1;
+}
+if (isset($opt['m']) || isset($opt['modechange'])) {
+    $copt['modechange'] = 1;
+}
 
-	try {
-		//get connection to database and call routine for owSwitchTimerControl
-		$dbh = new PDO($cfg['dsn'], $cfg['dbuser'], $cfg['dbpass']);
-		owSwitchTimerControl($dbh, $copt);
-	}
-	catch (PDOException $e) {
-		logEvent('Connection to database failed: ' . $e->getMessage(), LLWARN);
-	}
-	catch (Exception $e) {
-		logEvent("Error: {$e->getMessage()}", LLWARN);
-	}
+//one wire: init
+if (!init($cfg['ow_adapter'])) {
+    logEvent("cannot initialize 1-wire bus", LLWARN);
+    print("cannot initialize 1-wire bus.\n");
+    exit(9);
+}
 
-	logEvent("program ".__FILE__." finished", LLINFO);
+try {
+    //get connection to database and call routine for owSwitchTimerControl
+    $dbh = new PDO($cfg['dsn'], $cfg['dbuser'], $cfg['dbpass']);
+    owSwitchTimerControl($dbh, $copt);
+} catch (PDOException $e) {
+    logEvent('Connection to database failed: ' . $e->getMessage(), LLWARN);
+} catch (Exception $e) {
+    logEvent("Error: {$e->getMessage()}", LLWARN);
+}
+
+logEvent("program " . __FILE__ . " finished", LLINFO);
